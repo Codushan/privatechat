67f7a53758b5cff6419df93c9f5564b6a66a19d9
CB <chandrabhushankumar138@gmail.com>
2025-05-29 19:29:32 +0530

    fixes butterflies

--- lib/models/message.ts
+++ lib/models/message.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export interface IMessage {
-  _id?: string; // Optional for new messages
+  _id: string;
   sender: string;
   content: string;
   contentType: 'text' | 'image' | 'video' | 'document' | 'gif' | 'sticker';
